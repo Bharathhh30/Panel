a34c55fa25bad58761754abf126c52fb463ded95
Bharathhh30 <vnsb30@gmail.com>
2025-03-03 22:19:25 +0530

    added layout (_layout.tsx)

---
+++ app/(nobottombar)/accountinfo.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+
+export default function accountInfo(){
+    return <SafeAreaView>
+        <Text>Account info</Text>
+    </SafeAreaView>
+}
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,8 +1,12 @@
+import { Link } from "expo-router";
 import { View,Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function explore(){
     return <SafeAreaView>
         <Text>Explore Page</Text>
+        <Link href={"/accountinfo"}>
+            <Text>Go to Account Info</Text>
+        </Link>
     </SafeAreaView>
 }
