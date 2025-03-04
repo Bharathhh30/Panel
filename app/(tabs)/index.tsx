20b6266ed38ff2b784d21bf67631c989dbf1ad41
Bharathhh30 <vnsb30@gmail.com>
2025-03-04 18:57:58 +0530

    Added Topbars (bruteforce)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,20 @@
+import { Link } from "expo-router";
 import { View,Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function foryou(){
     return <SafeAreaView>
+        <Link href={"/liked"}>
+            <Text>Liked</Text>
+        </Link>
+
+        <Link href={"/suggested"}>
+            <Text>Suggested</Text>
+        </Link>
+
+        <Link href={"/library"}>
+            <Text>Library</Text>
+        </Link>
         <Text>For You Page</Text>
     </SafeAreaView>
 }
