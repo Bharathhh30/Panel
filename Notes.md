b899139afd17507d3334141bad162d7a002eb4d4
Bharathhh30 <vnsb30@gmail.com>
2025-03-04 20:20:44 +0530

    Stack Navigation (stack.screen)

--- Notes.md
+++ Notes.md
@@ -3,3 +3,5 @@ When something is not working *just reload*
 If using **Expo** We have to install / run the commands mentioned for expo alsoo 
 
 ```you have to wrap the entire app with gesture handler not just the component```
+
+*headerBackTitle wont work for andriod , works only for ios*
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,28 @@
-import { Slot } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 export default function Layout(){
     return     <GestureHandlerRootView>
-        <Slot/>
+        {/* <Slot/> */}
+        <Stack screenOptions={{
+            headerShown: false
+        }}
+        >
+            <Stack.Screen name="(nobottombar)/accountinfo" options={{headerShown:true,
+                headerTitle : "Account Info",
+                headerBackTitle : "Go Back",
+            }} />
+        </Stack>
     </GestureHandlerRootView>
 
 }
 
+// if we are using stacks from expo router no need to use slot
 
 // you have to wrap the entire app with gesture handler not just the component
+
+
+// stack lo headershown false pedthe whole app ki false ayipothadi , Stack.Screen use cheste we can enable the header only for that screen
+
+// headerBackTitle wont work for andriod , works only for ios
